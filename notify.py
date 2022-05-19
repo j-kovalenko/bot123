a590353cad880f9d9701e919ce6ba2f4b940de58
j-kovalenko <stdnkp@gmail.com>
2022-05-19 13:22:42 +0300

    edit notification

--- notify.py
+++ notify.py
@@ -3,7 +3,7 @@ from app import notification
 import time
 
 schedule.every().day.at("09:00").do(notification)
-schedule.every().day.at("14:00").do(notification)
+schedule.every().day.at("17:00").do(notification)
 
 while True:
     schedule.run_pending()
